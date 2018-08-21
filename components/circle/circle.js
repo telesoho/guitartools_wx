248187067552f942f2aee9daac040b00b61238e6
telesoho <telesoho@gmail.com>
2018-08-21 15:57:27 +0900

    完善computed功能

--- components/circle/circle.js
+++ components/circle/circle.js
@@ -54,7 +54,8 @@ Component({
    */
   methods: {
     onTap: function () {
-      this.setData({test:"new", test2: this.data.test2+1});
+      this.setData({'percent': this.data.percent + 1});
+      console.log(this.data.percent);
     }
   },
   created() {
@@ -69,28 +70,29 @@ Component({
       },
     }),
     computed(this, {
-      pic() {
-        return "background-image: url(\"data:image/svg+xml, %3Csvg viewBox='0 0 100 100' height='24' width='24' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M0 0h24v24H0z' fill='none'/%3E%3Cpath d='M19 19H5V5h7V3H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14c1.1 0 2-.9 2-2v-7h-2v7zM14 3v2h3.59l-9.83 9.83 1.41 1.41L19 6.41V10h2V3h-7z'/%3E%3C/svg%3E\");background-size: cover;";
+      circle_svg () {
+        return `data:image/svg+xml, \
+        %3Csvg viewBox='0 0 100 100' xmlns='http://www.w3.org/2000/svg'%3E\
+        %3Cpath d='${this.data.pathString}' stroke='${this.data.trailColor}' stroke-width='${this.data.trailWidth}' fill='0'/%3E\
+        %3Cpath d='${this.data.pathString}' stroke-linecap='${this.data.strokeLinecap}' \
+        stroke='${this.data.strokeColor}' stroke-width='${this.data.strokeWidth}' style='${this.data.pathStyle}' /%3E\
+        %3C/svg%3E`;
       },
       circleSize() {
-        console.log(this);
-        return {
-          width: `${this.data.size}px`,
-          height: `${this.data.size}px`
-        }
+        return `width: ${this.data.size}px;height: ${this.data.size}px`;
       },
       len() {
         return Math.PI * 2 * this.data.radius
       },
       pathStyle() {
-        return {
-          'stroke-dasharray': `${this.data.len}px ${this.data.len}px`,
-          'stroke-dashoffset': `${((100 - this.data.percent) / 100 * this.data.len)}px`
-        }
+        let ret = `stroke-dasharray: ${this.data.len}px ${this.data.len}px;\
+          stroke-dashoffset: ${((100 - this.data.percent) / 100 * this.data.len)}px`;
+        console.trace(ret);
+        return ret;
       },
       pathString() {
-        let ret = `M 50,50 m 0,-${this.data.radius}
-                    a ${this.data.radius},${this.data.radius} 0 1 1 0,${2 * this.data.radius}
+        let ret = `M 50,50 m 0,-${this.data.radius}\
+                    a ${this.data.radius},${this.data.radius} 0 1 1 0,${2 * this.data.radius}\
                     a ${this.data.radius},${this.data.radius} 0 1 1 0,-${2 * this.data.radius}`;
         return ret;
       },
--- utils/vuefy.js
+++ utils/vuefy.js
@@ -19,33 +19,104 @@ function watch(ctx, watchFunctions) {
  * @param {*} computedFunctions 
  */
 function computed(ctx, computedFunctions) {
-  let functionNames = Object.keys(computedFunctions)
-  functionNames.forEach(functionName => {
-    defineComputed(ctx, functionName, computedFunctions[functionName]);
+  let dataNames = Object.keys(ctx.data);
+  dataNames.forEach(dataName => {
+    defineDataReactive(ctx, dataName, ctx.data[dataName]);
   })
-  
+
+  let functionNames = Object.keys(computedFunctions);
+  functionNames.forEach(functionName => {
+    defineComputedReactive(ctx, functionName, computedFunctions[functionName]);
+  })
+
   // 将computed中的函数对象，追加到data数据中
   let firstComputedObj = functionNames.reduce((dataObj, functionName) => {
-    // cacl and set  data first time.
-    dataObj[functionName] = computedFunctions[functionName].call(ctx)
+    // set $_regist_function for registion data watch function
+    ctx.$_regist_function = {
+      funName: functionName,
+      funObj : function() {
+        ctx.setData({ [functionName]: computedFunctions[functionName].call(ctx) });
+      }
+    } 
+
+    // calc and set data first time.
+    dataObj[functionName] = computedFunctions[functionName].call(ctx);
+
+    // remove $_regist_function
+    ctx.$_regist_function = null;
     return dataObj;
   }, {});
   ctx.setData(firstComputedObj);
 }
 
+
 /**
  * define computed functions
  */
-function defineComputed(ctx, key, getFn) {
-  ctx['_$get_' + key] = getFn;
+function defineComputedReactive(ctx, key, fun) {
+  var val;
+  let watchList = [];
   Object.defineProperty(ctx.data, key, {
     configurable: true,
     enumerable: true,
     get: function () {
-      return ctx['_$get_' + key].call(ctx);
+      if (ctx.$_regist_function) {
+        // if has regist watch function, than regist it.
+        watchList[ctx.$_regist_function.funName] = ctx.$_regist_function.funObj
+      }
+      if(typeof val === 'undefined') {
+        val = fun.call(ctx);
+      }
+      return val;
     },
     set: function (newVal) {
-      // console.trace(key, newVal);
+      if (newVal === val) {
+        return
+      }
+      // if this computed has been changed than call watching functions.
+      let watchKeys = Object.keys(watchList);
+      if (watchKeys.length) {
+        // 用 setTimeout 因为此时 this.data 还没更新
+        setTimeout(() => {
+          watchKeys.forEach(sub => {
+            watchList[sub]();
+          })
+        }, 0)
+      }
+      val = newVal
+    },
+  })
+}
+
+
+/**
+ * define data watch functions
+ */
+function defineDataReactive(ctx, key, val) {
+  let watchList = [];
+  Object.defineProperty(ctx.data, key, {
+    configurable: true,
+    enumerable: true,
+    get: function () {
+      if (ctx.$_regist_function) {
+        // regist watch function
+        watchList[ctx.$_regist_function.funName] = ctx.$_regist_function.funObj
+      }
+      return val;
+    },
+    set: function (newVal) {
+      if (newVal === val) return
+      let watchKeys = Object.keys(watchList);
+      console.log(key, watchKeys);
+      if (watchKeys.length) {
+        // 用 setTimeout 因为此时 this.data 还没更新
+       setTimeout(() => {
+          watchKeys.forEach(sub => {
+            watchList[sub]();
+          })
+       }, 0)
+      }
+      val = newVal
     },
   })
 }
