aeecd9b889436f761bfaf6faa6eccda2d1c9c974
telesoho <telesoho@gmail.com>
2018-08-22 15:31:35 +0900

    支持动画

--- pages/index/index.js
+++ pages/index/index.js
@@ -7,13 +7,48 @@ Page({
     motto: 'Hello World',
     userInfo: {},
     hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    animationData: {}
   },
+  onShow: function(){
+    var animation = wx.createAnimation({
+      duration: 1000,
+  	  timingFunction: 'ease',
+    })
+
+    this.animation = animation
+  },  
   //事件处理函数
   bindViewTap: function() {
     wx.navigateTo({
       url: '../logs/logs'
     })
+  },
+  rotateAndScale: function () {
+    // 旋转同时放大
+    this.animation.rotate(45).scale(2, 2).step()
+    this.setData({
+      animationData: this.animation.export()
+    })
+  },
+  rotateThenScale: function () {
+    // 先旋转后放大
+    this.animation.rotate(45).step()
+    this.animation.scale(2, 2).step()
+    this.setData({
+      animationData: this.animation.export()
+    })
+  },
+  rotateAndScaleThenTranslate: function () {
+    // 先旋转同时放大，然后平移
+    this.animation.rotate(45).scale(2, 2).step()
+    this.animation.translate(100, 100).step({ duration: 1000 })
+    this.setData({
+      animationData: this.animation.export()
+    })
+  },
+  onLongPress() {
+    this.rotateAndScale();
   },
   onCircleTap: function () {
     let myc  = this.selectComponent('#myc');
